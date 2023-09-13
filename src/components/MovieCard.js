75ea7e023b8208f7da7b67996d4ab03dd80c5789
Mwadali <wellingtonmwadali@gmail.com>
2023-09-13 23:12:08 +0300

    homepage backdrop, movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaHeart } from 'react-icons/fa';
+import { FaImdb} from 'react-icons/fa';
+import { toast } from 'react-hot-toast';
 
 function MovieCard({ movie }) {
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    const toggleFavorite = () => {
+      setIsFavorite(!isFavorite);
+      // Show an alert message when the heart icon is clicked
+      toast(isFavorite ? 'Removed from favorites' : 'Added to favorites');
+      
+    };
   return (
-    <div className="bg-white p-4 rounded-lg shadow-md ">
+    <div className="bg-white lg:px-5 lg:py-5">
       <Link to={`/movies/${movie.id}`}>
         <img
           src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
           alt={movie.title}
-          className="rounded-lg hover:opacity-80 transition-opacity duration-300"
-          style={{ maxHeight: '300px' }}
+          className="hover:opacity-80 transition-opacity duration-300 "
+          style={{ maxHeight: '270px', maxWidth:'300px'  }}
         />
         <h2 className="text-lg font-semibold mt-2">{movie.title}</h2>
-        <p className="text-sm text-gray-500">{movie.release_date}</p>
-      </Link>
+        </Link>
+        <p className="text-sm text-gray-500 flex"><span className='text-xl'><FaImdb/></span>{movie.release_date}</p>
+        <div className='flex gap-12'>
+        <p className="text-sm"> Rating: {movie.vote_average} / 10</p>
+        <div className="cursor-pointer" onClick={toggleFavorite}>
+          <FaHeart
+            color={isFavorite ? 'red' : 'gray'}
+            size={16}
+            className="transition-colors duration-300  hover:text-red-500"/>
+        </div>
+        </div>
     </div>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,10 +3,22 @@ import axios from 'axios';
 import MovieCard from '../components/MovieCard';
 
 function Homepage() {
+  const [featuredMovie, setFeaturedMovie] = useState(null);
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const fetchFeaturedMovie = async () => {
+      try {
+        const apiKey = '4fcdd94c5f3104840d062de0e3501eee';
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/458156-john-wick-chapter-3-parabellum?api_key=${apiKey}`
+        );
+        setFeaturedMovie(response.data);
+      } catch (error) {
+        console.error('Error fetching featured movie:', error);
+      }
+    };
     const fetchTopMovies = async () => {
       try {
         const apiKey = '4fcdd94c5f3104840d062de0e3501eee';
@@ -21,16 +33,41 @@ function Homepage() {
       }
     };
 
+    fetchFeaturedMovie();
     fetchTopMovies();
   }, []);
 
   return (
     <div>
-      <h1 className='font-bold px-4'>Top 10 Movies</h1>
+    {featuredMovie && (
+      <div className="shadow-md">
+       
+        <div
+              className="bg-cover bg-center"
+              style={{
+                backgroundImage: `url('https://image.tmdb.org/t/p/original/${featuredMovie.backdrop_path}')`,
+                height: '430px',
+              }}
+            >
+              <div className="lg:p-4">
+                <p className="text-lg font-semibold text-white">{featuredMovie.title}</p>
+                <p className="text-sm text-gray-300 mb-4">Release Date: {featuredMovie.release_date}</p>
+                
+                <div className="w-1/4">
+                <p className="text-lg font-semibold text-white">Overview</p>
+                <p className="text-sm text-gray-300">{featuredMovie.overview}</p>
+                </div>
+              </div>
+            </div>
+       
+        </div>
+      
+    )}
+      <h1 className='font-bold lg:px-4'>Featured Movies</h1>
       {loading ? (
-        <div>Loading...</div>
+        <div className='text-center'>Loading...</div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-1">
           {movies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
