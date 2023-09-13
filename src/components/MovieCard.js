5571ace6c92d034b39c67996f1d310587a805498
Mwadali <wellingtonmwadali@gmail.com>
2023-09-13 14:24:42 +0300

    Movie card styles

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 
 function MovieCard({ movie }) {
   return (
-    <div className="bg-white p-4 rounded-lg shadow-md">
+    <div className="bg-white p-4 rounded-lg shadow-md ">
       <Link to={`/movies/${movie.id}`}>
         <img
           src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
           alt={movie.title}
           className="rounded-lg hover:opacity-80 transition-opacity duration-300"
+          style={{ maxHeight: '300px' }}
         />
         <h2 className="text-lg font-semibold mt-2">{movie.title}</h2>
         <p className="text-sm text-gray-500">{movie.release_date}</p>
