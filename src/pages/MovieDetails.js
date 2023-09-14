2981fea3638198e7f62d714dad1300fee34ffe8e
Mwadali <wellingtonmwadali@gmail.com>
2023-09-15 02:36:23 +0300

    removed loading '

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
+import Sidebar from '../components/Sidebar'; // Ensure the correct import path
 
 function MovieDetails() {
   const { id } = useParams();
   const [movie, setMovie] = useState({});
-  const [setLoading] = useState(true);
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -16,10 +15,8 @@ function MovieDetails() {
           `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`
         );
         setMovie(response.data);
-        setLoading(false);
       } catch (error) {
         console.error('Error fetching movie details:', error);
-        setLoading(false);
       }
     };
 
@@ -28,38 +25,34 @@ function MovieDetails() {
 
   return (
     <div className='flex'>
-       <Sidebar/>
-    {/* Movie Backdrop Image banner*/}
-    <div>
-    <img
-      src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} 
-      alt={movie.title}
-      className="shadow-md rounded-md h-96 mx-auto"
-    />
+      <Sidebar />
+      <div>
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          alt={movie.title}
+          className="shadow-md rounded-md h-96 mx-auto"
+        />
 
-    {/* Movie Details */}
-    <div className='text-black text-center'>
-      <h1 className="text-3xl font-semibold my-4" data-testid="movie-title">
-        {movie.title}
-      </h1>
-      <p className="text-sm" data-testid="movie-release-date">
-        Release Date: {movie.release_date}
-      </p>
-      <p className="text-sm font-bold" data-testid="movie-runtime">
-        Runtime: {movie.runtime} minutes
-      </p>
-      <div className='flex items-center justify-center'>
-        <p className="text-sm my-4 font-medium lg:w-1/2 bg-slate-300 rounded-md" data-testid="movie-overview">
-          {movie.overview}
-        </p>
+        <div className='text-black text-center'>
+          <h1 className="text-3xl font-semibold my-4">{movie.title}</h1>
+          <p className="text-sm" data-testid="movie-release-date">
+            Release Date: {movie.release_date}
+          </p>
+          <p className="text-sm font-bold" data-testid="movie-runtime">
+            Runtime: {movie.runtime} minutes
+          </p>
+          <div className='flex items-center justify-center'>
+            <p className="text-sm my-4 font-medium lg:w-1/2 bg-slate-300 rounded-md" data-testid="movie-overview">
+              {movie.overview}
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-   
-  </div>
     </div>
   );
 }
 
 export default MovieDetails;
+
 
 
